398824227c51efc5551b9da737a0da5ad83f4247
Dolpheyn <farissufyan99@gmail.com>
2019-12-08 17:45:41 +0800

    Added xss that sends uname and pwd to remote machine

---
+++ post-to-attacker/search.php
@@ -0,0 +1,16 @@
+<?php
+$languages = array(
+	"java"=>"James Gosling",
+	"c++"=>"Bjarne Stroustrup",
+	"c#"=>"Microsoft",
+	"php" => "Rasmus Lerdorf",
+	"javascript" => "Brendan Eich");
+
+$query = $_GET['search'];
+
+if(array_key_exists(strtolower($query), $languages)){
+	echo $query . " is designed by " . $languages[$query];
+}
+else{
+	echo $query . " not found.";
+}
